8d1751004dc65062e0a4ac87e6878c9c814b83f6
Ryan Echols <rkechols21@gmail.com>
2019-04-11 11:55:08 -0600

    Regex for 'ли' and 'бы' functional

--- src/main/java/com/flair/server/parser/StanfordDocumentParserRussianStrategy.java
+++ src/main/java/com/flair/server/parser/StanfordDocumentParserRussianStrategy.java
@@ -1,12 +1,14 @@
 /*
  * This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License.
  * To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/.
- 
+
  */
 package com.flair.server.parser;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.flair.server.grammar.RussianGrammaticalTreePatterns;
 import com.flair.server.utilities.ServerLogger;
@@ -15,6 +17,7 @@ import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.semgraph.SemanticGraph;
 import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeCoreAnnotations;
@@ -26,26 +29,26 @@ import edu.stanford.nlp.util.CoreMap;
 
 
 class StanfordDocumentParserRussianStrategy extends BasicStanfordDocumentParserStrategy {
-	private AbstractDocument workingDoc;
-	private int tokenCount;
-	private int wordCount;
-	private int characterCount;
-	private int sentenceCount;
-	private int depthCount;
-	private int dependencyCount;
-	private int adjCount;
-	private static final String WORD_PATTERN = "[\\p{IsCyrillic}\u0300\u0301]+"; //not sure if this regex is correct for including all number of russian words. EDIT: regex has been changed to handle the two accents over letters
+    private AbstractDocument workingDoc;
+    private int tokenCount;
+    private int wordCount;
+    private int characterCount;
+    private int sentenceCount;
+    private int depthCount;
+    private int dependencyCount;
+    private int adjCount;
+    private static final String WORD_PATTERN = "[\\p{IsCyrillic}\u0300\u0301]+"; //not sure if this regex is correct for including all number of russian words. EDIT: regex has been changed to handle the two accents over letters
 
     public StanfordDocumentParserRussianStrategy()
     {
-    
-	//pipeline = null;
+
+        //pipeline = null;
     }
-    
+
     public void setPipeline(StanfordCoreNLP pipeline)
     {
-    	assert pipeline != null;
-    	this.pipeline = pipeline;
+        assert pipeline != null;
+        this.pipeline = pipeline;
     }
 
     public boolean	isLanguageSupported(Language lang){
@@ -53,18 +56,18 @@ class StanfordDocumentParserRussianStrategy extends BasicStanfordDocumentParserS
         //return (lang == Language.RUSSIAN);
     }
     private void initializeState(AbstractDocument doc) {
-		if (pipeline == null)
-		{
-			throw new IllegalStateException("Parser not set");
-		} else if (!isLanguageSupported(doc.getLanguage()))
-		{
-			throw new IllegalArgumentException("Document language " + doc.getLanguage()
-					+ " not supported (Strategy language: " + Language.RUSSIAN + ")");
-		}
+        if (pipeline == null)
+        {
+            throw new IllegalStateException("Parser not set");
+        } else if (!isLanguageSupported(doc.getLanguage()))
+        {
+            throw new IllegalArgumentException("Document language " + doc.getLanguage()
+                    + " not supported (Strategy language: " + Language.RUSSIAN + ")");
+        }
 
-		workingDoc = doc;
-	}
-    
+        workingDoc = doc;
+    }
+
     private void resetState() {
 		/*wordCount = tokenCount = dependencyCount = sentenceCount = depthCount = characterCount = goingToFound = 0;
 		treeOutput = null;
@@ -74,109 +77,120 @@ class StanfordDocumentParserRussianStrategy extends BasicStanfordDocumentParserS
 		usedFound = false;
 		comparativeMoreFound = false;
 		superlativeMostFound = false;*/
-    	wordCount = tokenCount = characterCount = sentenceCount = dependencyCount = depthCount = 0;
-		pipeline = null;
-		workingDoc = null;
-	}
-
-    private void inspectSentence(Tree tree, List<CoreLabel> words) {
-        if(tree == null){
-            ServerLogger.get().info("Received a null tree to inspect sentence in the RussianStrategy");
-            return;
-        }
-        if (words == null || words.isEmpty()) {
-            return;
-        }
-        int numLIs = countMatches(RussianGrammaticalTreePatterns.patternLi, tree);
-        int numConditionals = countMatches(RussianGrammaticalTreePatterns.patternBi, tree);
+        wordCount = tokenCount = characterCount = sentenceCount = dependencyCount = depthCount = 0;
+        pipeline = null;
+        workingDoc = null;
     }
-    
+
     public boolean	apply(AbstractDocument docToParse){ //TODO: edit this to recognize li and buj/bi sentences
-		assert docToParse != null;
-		int attempts = 0; 
-		try
-		{
-			initializeState(docToParse);
+        assert docToParse != null;
+        int attempts = 0;
+        try
+        {
+            initializeState(docToParse);
 
-			Annotation docAnnotation = new Annotation(workingDoc.getText());
-			pipeline.annotate(docAnnotation);
+            Annotation docAnnotation = new Annotation(workingDoc.getText());
+            pipeline.annotate(docAnnotation);
 
-			List<CoreMap> sentences = docAnnotation.get(CoreAnnotations.SentencesAnnotation.class);
-			for (CoreMap itr : sentences)
-			{
+            List<CoreMap> sentences = docAnnotation.get(CoreAnnotations.SentencesAnnotation.class);
+            for (CoreMap itr : sentences)
+            {
 				/*if(attempts % 20 == 0){
 					ServerLogger.get().info("Parsing " + docToParse.getDescription() + "...");
 				}*/
-
-				if (itr.size() > 0)
-				{
-					Tree tree = itr.get(TreeCoreAnnotations.TreeAnnotation.class);
-					List<CoreLabel> words = itr.get(CoreAnnotations.TokensAnnotation.class);
+                if (itr.size() > 0)
+                {
+                    SemanticGraph graph = itr.get(SemanticGraphCoreAnnotations.EnhancedPlusPlusDependenciesAnnotation.class);
+                    List<CoreLabel> words = itr.get(CoreAnnotations.TokensAnnotation.class);
 					/*Collection<TypedDependency> dependencies = itr
 							.get(SemanticGraphCoreAnnotations.CollapsedDependenciesAnnotation.class)
 							.typedDependencies();
 							*/
 
-					inspectSentence(tree, words);
+                    inspectSentence(graph, words);
 
+                    sentenceCount++;
+                    //dependencyCount += dependencies.size();
+                    //depthCount += tree.depth();
 
-					sentenceCount++;
-					//dependencyCount += dependencies.size();
-					//depthCount += tree.depth();
+                    // changed: only count words (no punctuation)
+                    for (CoreLabel cl : words)
+                    {
+                        tokenCount++;
+                        if (!cl.tag().startsWith("$"))
+                        {
+                            dependencyCount += 1;
+                        }
+                        if (cl.value().toLowerCase().matches(WORD_PATTERN))
+                        {
+                            wordCount++;
+                            characterCount += cl.value().length();
+                        }
+                    }
 
-					// changed: only count words (no punctuation)
-					for (CoreLabel cl : words)
-					{
-						tokenCount++;
-						if (!cl.tag().startsWith("$"))
-						{
-							dependencyCount += 1;
-						}		
-						if (cl.value().toLowerCase().matches(WORD_PATTERN))
-						{
-							wordCount++;
-							characterCount += cl.value().length();
-						}
-					}
+                    // extract gram.structures
+                    //inspectSentence(tree, words, dependencies);
+                }
+            }
 
-					// extract gram.structures
-					//inspectSentence(tree, words, dependencies);
-				}
-			}
-
-			// update doc properties
-			workingDoc.setAvgSentenceLength((double) wordCount / (double) sentenceCount);
-			//workingDoc.setAvgTreeDepth((double) depthCount / (double) sentenceCount);
-			workingDoc.setAvgWordLength((double) characterCount / (double) wordCount);
-			workingDoc.setLength(wordCount);
-			workingDoc.setNumDependencies(dependencyCount);
-			workingDoc.setNumWords(wordCount);
-			workingDoc.setNumTokens(tokenCount);
-			workingDoc.setNumSentences(sentenceCount);
-			workingDoc.setNumCharacters(characterCount);
-			workingDoc.flagAsParsed();
-		} finally
-		{
-			resetState();
-		}
+            // update doc properties
+            workingDoc.setAvgSentenceLength((double) wordCount / (double) sentenceCount);
+            //workingDoc.setAvgTreeDepth((double) depthCount / (double) sentenceCount);
+            workingDoc.setAvgWordLength((double) characterCount / (double) wordCount);
+            workingDoc.setLength(wordCount);
+            workingDoc.setNumDependencies(dependencyCount);
+            workingDoc.setNumWords(wordCount);
+            workingDoc.setNumTokens(tokenCount);
+            workingDoc.setNumSentences(sentenceCount);
+            workingDoc.setNumCharacters(characterCount);
+            workingDoc.flagAsParsed();
+        } finally
+        {
+            resetState();
+        }
 
         return true;
     }
 
-	/**
-	 * Counts the number of matches to a specific TregexPattern within a parse tree
-	 * @param pattern TregexPattern to be matched against the Tree
-	 * @param tree Dependency tree
-	 * @return number of matches to the tregex pattern
-	 */
-	private int countMatches(TregexPattern pattern, Tree tree) {
-		int matches = 0;
-		TregexMatcher matcher = pattern.matcher(tree);
-		while (matcher.findNextMatchingNode())	//while the matcher can find the next match to the pattern, increment the number of matches
-		{
-			matches++;
-		}
-		return matches;
-	}
+    private void inspectSentence(SemanticGraph graph, List<CoreLabel> words) {
+        if (words == null || words.isEmpty()) {
+            return;
+        }
+        int numLIs = countMatches(RussianGrammaticalTreePatterns.patternLi, words);
+        int numConditionals = countMatches(RussianGrammaticalTreePatterns.patternBi, words);
+        ServerLogger.get().info("Number of 'ли' words found: " + numLIs);
+        ServerLogger.get().info("Number of 'бы' words found: " + numConditionals);
+        //TODO
+    }
 
+    /**
+     * Counts the number of matches to a specific regex Pattern within a parse graph
+     * @param pattern Pattern to be matched against the graph
+     * @param graph Dependency graph
+     * @return number of matches to the regex pattern
+     */
+    private int countMatches(Pattern pattern, SemanticGraph graph) {
+        int matches = 0;
+        //graph.getAllNodesByWordPattern()
+        //TODO
+        return matches;
+    }
+
+    /**
+     * Counts the number of matches to a specific regex Pattern for individual words
+     * @param pattern Pattern to be matched against each word
+     * @param words   List of models that contain individual word values
+     * @return number of matches to the regex pattern
+     */
+    private int countMatches(Pattern pattern, List<CoreLabel> words){
+        int matches = 0;
+        for(CoreLabel word : words){
+            final String wordValue = word.value();
+            Matcher m = pattern.matcher(wordValue);
+            while(m.find()){
+                matches++;
+            }
+        }
+        return matches;
+    }
 }
