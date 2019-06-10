80a8f1ad653c479735e8d10482ad4662ba0132c5
Ryan Echols <rkechols21@gmail.com>
2019-06-10 20:03:09 +0200

    Trying to load cg-conv

--- src/main/java/com/flair/server/parser/StanfordDocumentParserRussianStrategy.java
+++ src/main/java/com/flair/server/parser/StanfordDocumentParserRussianStrategy.java
@@ -215,6 +215,7 @@ class StanfordDocumentParserRussianStrategy extends BasicStanfordDocumentParserS
         }
 
 
+
         int numLIs = countMatches(RussianGrammaticalPatterns.patternLi, words);
         int numConditionals = countMatches(RussianGrammaticalPatterns.patternBi, words);
         inspectVerbs(graph, words);
