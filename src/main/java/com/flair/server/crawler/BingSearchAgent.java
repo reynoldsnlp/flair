649a67cdf816d7c3bf187a64ad52a0371705a047
mjbriggs <mjbriggs@icloud.com>
2019-06-26 12:02:02 -0400

    add logging for failed environment variable grab

--- src/main/java/com/flair/server/crawler/BingSearchAgent.java
+++ src/main/java/com/flair/server/crawler/BingSearchAgent.java
@@ -15,17 +15,37 @@ import com.flair.shared.grammar.Language;
  */
 class BingSearchAgent extends CachingSearchAgent
 {
-	private static final String		 PROD_API_KEY		= System.getenv("BING_API");
+	private static String		 PROD_API_KEY;		
 	
 	private static final int		RESULTS_PER_PAGE	= 100;		// larger numbers will reduce the number of search transactions but will increase the response size
 	private static final int		MAX_API_REQUESTS	= 2;
 
 	private final AzureWebSearch pipeline;
+
+	private static boolean 			isKeySet;
 	
 	public BingSearchAgent(Language lang, String query)
 	{
 		super(lang, query, MAX_API_REQUESTS);
 		this.pipeline = new AzureWebSearch();
+		isKeySet = false;
+
+		try{			
+			if(!isKeySet) {
+				ServerLogger.get().info("Setting PROD_API_KEY");
+				PROD_API_KEY = System.getenv("BING_API");
+				isKeySet = true;
+			}
+			else {
+				throw new Exception();
+			}
+		}
+		catch(SecurityException ex) {
+			ServerLogger.get().error(ex, "caught security exception trying to grab BING_API variable");
+		}
+		catch(Exception ex) {
+			ServerLogger.get().error(ex, "caught exception trying to grab BING_API variable");
+		}
 
 		String qPrefix = "";
 		String qPostfix = " language:en";
