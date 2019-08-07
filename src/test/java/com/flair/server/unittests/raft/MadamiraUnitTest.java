a86279455978986514d238c6dd09715f15e2a4cf
mjbriggs <mjbriggs@icloud.com>
2019-08-07 14:14:50 -0400

    removed unused imports

--- src/test/java/com/flair/server/unittests/raft/MadamiraUnitTest.java
+++ src/test/java/com/flair/server/unittests/raft/MadamiraUnitTest.java
@@ -1,18 +1,11 @@
 package com.flair.server.unittests.raft;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.lang.Thread;
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.TimeUnit;
 
 import com.flair.server.raft.Madamira;
 import com.flair.server.utilities.CustomFileReader;
