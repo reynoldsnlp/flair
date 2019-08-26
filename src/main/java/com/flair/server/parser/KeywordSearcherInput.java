104c4f21cf9ba103cf7f0670caa847f70e3e2e78
mjbriggs <mjbriggs@icloud.com>
2019-08-26 08:22:12 -0400

    added getter, trivial style changes

--- src/main/java/com/flair/server/parser/KeywordSearcherInput.java
+++ src/main/java/com/flair/server/parser/KeywordSearcherInput.java
@@ -14,37 +14,51 @@ import java.util.List;
 public class KeywordSearcherInput implements Iterable<String>
 {
     private final List<String>		    keywords;
-    private final boolean		    debugMode;	    // prints debug output
+    private final boolean		    	debugMode;	    // prints debug output
 
-    public KeywordSearcherInput() {
-	this.keywords = new ArrayList<>();
-	this.debugMode = false;
+    public KeywordSearcherInput() 
+    {
+    	this.keywords = new ArrayList<>();
+    	this.debugMode = false;
     }
     
-    public KeywordSearcherInput(List<String> keywords) {
-	this.keywords = new ArrayList<>(keywords);
-	this.debugMode = false;
+	public boolean isDebugMode() {
+		return debugMode;
+	}
+
+	public KeywordSearcherInput(List<String> keywords) 
+    {
+    	this.keywords = new ArrayList<>(keywords);
+    	this.debugMode = false;
     }
     
-    public KeywordSearcherInput(List<String> keywords, boolean debugMode) {
-	this.keywords = new ArrayList<>(keywords);
-	this.debugMode = debugMode;
+    public KeywordSearcherInput(List<String> keywords, boolean debugMode) 
+    {
+    	this.keywords = new ArrayList<>(keywords);
+    	this.debugMode = debugMode;
     }
     
     public void addKeyword(String keyword) 
     {
-	// force to lowercase
-	keyword = keyword.toLowerCase();
-	if (keywords.contains(keyword) == false)
-	    keywords.add(keyword);
+    	// force to lowercase
+    	keyword = keyword.toLowerCase();
+    	if (keywords.contains(keyword) == false)
+    		keywords.add(keyword);
     }
     
     @Override
-    public Iterator<String> iterator() {
-	return keywords.iterator();
+    public Iterator<String> iterator() 
+    {
+    	return keywords.iterator();
     }
     
-    public boolean isDebuggable() {
-	return debugMode;
+    public boolean isDebuggable() 
+    {
+    	return debugMode;
     }
+    
+    public List<String> getKeywords() 
+    {
+		return keywords;
+	}
 }
