6e06d1b1d72d7b96130a8036910a39a9daac76df
Rob Reynolds <rob@icall.byu.edu>
2022-02-08 10:50:44 -0700

    update deploy script

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,12 +1,18 @@
+logfile="deploy_prod.out"
+rm ${logfile}
+touch ${logfile}
+
+echo "Sending output to ${logfile} ..."
+
 # if reynoldsnlp/flair-base image does not exist, build it
-docker inspect reynoldsnlp/flair-base 2> /dev/null \
-	|| docker build -t reynoldsnlp/flair-base -f Dockerfile_base .
-docker build -t reynoldsnlp/flair --no-cache -f Dockerfile .
+docker inspect reynoldsnlp/flair-base 2>&1 >> ${logfile} \
+	|| docker build -t reynoldsnlp/flair-base -f Dockerfile_base . 2>&1 >> ${logfile}
+docker build -t reynoldsnlp/flair --no-cache -f Dockerfile . 2>&1 >> ${logfile}
 
 # if `flair` container is already running, stop/rm the old one
-docker inspect flair 2> /dev/null \
-	&& docker stop flair \
-	&& docker rm flair
+docker inspect flair 2>&1 >> ${logfile} \
+	&& docker stop flair 2>&1 >> ${logfile} \
+	&& docker rm flair 2>&1 >> ${logfile}
 
 # run the `flair` container
-docker run -dit -p 8080:8080 -e BING_API=$BING_API --name flair --restart always reynoldsnlp/flair
+docker run -dit -p 8080:8080 -e BING_API=$BING_API --name flair --restart always reynoldsnlp/flair 2>&1 >> ${logfile}
